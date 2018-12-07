0e78331f32c45649f91efcae8421683e73ccc12c
Jessie Dembe <jmdembe@gmail.com>
2018-12-06 23:07:48 -0500

    created footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import Link from 'gatsby-link';
+
+const Footer = () => (
+    <div style={{background: '#5F4B8B', marginBottom: '1.45rem'}}>
+
+    </div>
+)
+
+export default Footer
