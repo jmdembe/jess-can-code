571b9930dc5d4b367fe71463e91f0cb1241b434b
Jessie Dembe <jmdembe@gmail.com>
2018-12-15 15:30:21 -0500

    added name to footer with link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-const Footer = () => (
-    <footer style={{background: '#5F4B8B', height: '150px', marginTop: '20px'}}>
-
+const Footer = ({ siteTitle }) => (
+    <footer style={{background: '#5F4B8B', marginTop: '20px'}}>
+        <div>
+            <p style={{margin: 0}}>
+                <Link to="/" style={{color: 'white',textDecoration: 'none',}}> { siteTitle } </Link>
+            </p>
+        </div>
     </footer>
 )
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -20,7 +20,7 @@ const Layout = ({ children, data }) => (
       {children()}
       
     </div>
-    <Footer/>
+    <Footer siteTitle ={data.site.siteMetadata.title} />
   </div>
   
 )
