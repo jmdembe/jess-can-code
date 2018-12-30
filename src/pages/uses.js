39f6af1371acc3520eaeff8f064a001adb0ae2fc
Jessie Dembe <jmdembe@gmail.com>
2018-12-30 13:28:04 -0500

    updated uses page

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -4,7 +4,15 @@ import Layout from "../components/layout"
 export default () => (
     <Layout>
         <div>
-            <h2>I use...</h2>
+            <h2>Things I use..</h2>
+            <p>This is a running page of tools that I use inspired to make a page of things I use like the ones used by:</p>
+            <ul>
+                <li>
+                    <a href="https://zen-of-programming.com/uses">Ali Spittel</a>
+                    <a href="https://www.bencodezen.io/uses/">Ben Hong</a>
+                    <a href="https://wesbos.com/uses/">Wes Bos</a>
+                </li>
+            </ul>
         </div>
     </Layout>
     
