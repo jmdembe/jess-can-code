6853ff9f672c9e102721801782426775b86abca3
Jessie Dembe <jmdembe@gmail.com>
2018-12-30 15:25:31 -0500

    Built out my uses page!

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -9,9 +9,41 @@ export default () => (
             <ul>
                 <li>
                     <a href="https://zen-of-programming.com/uses">Ali Spittel</a>
+                </li>
+                <li>
                     <a href="https://www.bencodezen.io/uses/">Ben Hong</a>
+                </li>
+                <li>
                     <a href="https://wesbos.com/uses/">Wes Bos</a>
                 </li>
+            </ul>
+
+            <p>I am doing this because I am curious in other people's setups, so I want to share in that content! The theme you will see with my setup
+            is that I separate the look of my home and work setup because when I am at home, it is more for fun and on my pet projects. For work, I like
+            to set up the mindset that I am working and to not deviate from my work tasks. With that, off we go!</p>
+
+            <h3>Hardware</h3>
+            <ul>
+                <li>Personal: I bought my <a href="https://www.techradar.com/reviews/macbook-pro">
+                15-inch MacBook Pro in November 2018</a> after my <a href ="https://everymac.com/systems/apple/macbook_pro/specs/macbook-pro-core-i5-2.4-13-late-2011-unibody-thunderbolt-specs.html">13-inch MacBook Pro (late 2011)</a> became so slow that I could not handle coding on it anymore.
+                I am not sad about it, it took me through my last two years of college, my early professional career,
+                and a coding bootcamp. It still works. My new laptop is a lot faster with a lot more screen real estate. It is like night and day.</li>
+                <li>Work: I use a <a href="https://www.laptopmag.com/reviews/laptops/dell-latitude-e5470">
+                    Dell Latitude E5470
+                </a> as my client also uses Windows machines. Also, I am a huge fan of separation of concerns when it comes to work and personal coding.
+                It works fine and it does the job, but I prefer Macs over PCs because there is less work setting up my environment, as it is on any *nix
+                machine. </li>
+                <li>Monitor: I do not have a formal desk setup, so I use a portable monitor to make a desk! The one I use is
+                this <a href="https://www.amazon.com/ASUS-MB169B-1920x1080-Portable-Monitor/dp/B013XFJKGI/ref=asc_df_B013XFJKGI/?tag=hyprod-20&linkCode=df0&hvadid=309751315916&hvpos=1o1&hvnetw=g&hvrand=2000356274849756023&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1018598&hvtargid=aud-466360936450:pla-383890328341&psc=1&tag=&ref=&adgrpid=67183599252&hvpone=&hvptwo=&hvadid=309751315916&hvpos=1o1&hvnetw=g&hvrand=2000356274849756023&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1018598&hvtargid=aud-466360936450:pla-383890328341">ASUS monitor</a>.</li>
+            </ul>
+            <h3>Software</h3>
+            <ul>
+                <li>Text Editor: My go-to text editor is <a href="https://code.visualstudio.com/">VSCode</a>. I have tried Atom and it was too slow on any of my machines.
+                I also did not like all the setup required on Sublime.</li>
+                <ul>
+                    <li>Theme: I use <a href="https://github.com/minamarkham/yonce">Yonc&eacute;</a> for VSCode and for everything else on my personal laptop. On my work machine, I use the 
+                    <a href="https://marketplace.visualstudio.com/items?itemName=Equinusocio.vsc-material-theme">Material Theme family</a>.</li>
+                </ul>
             </ul>
         </div>
     </Layout>
